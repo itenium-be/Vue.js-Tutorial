0e3f289c8ff61c4848ee91ce0cc470025d4a3b73
steven.robijns <robijns.steven@gmail.com>
2019-03-28 22:26:20 +0100

    4B: added productlines

---
+++ cli-socks/src/components/ProductLine.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>{{product.name}}
+        <router-link :to="{ name: 'product', params: { id: product.name }}">Link</router-link>
+    </div>
+</template>
+
+
+<script lang="ts">
+    import { Component, Prop, Vue } from "vue-property-decorator";
+    import { ProductModel } from '../models';
+
+    @Component({
+        components: {
+        }
+    })
+
+    export default class ProductLine extends Vue {
+        @Prop({default: undefined}) private product!: ProductModel;
+    }
+</script>
+
+<style scoped>
+
+</style>
--- cli-socks/src/views/Home.vue
+++ cli-socks/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <Product :premium="premium" @add-to-cart="updateCart"></Product>
+    <!--<Product :premium="premium" @add-to-cart="updateCart"></Product>-->
+    <ProductLine v-for="k in products" :key="k.name" :product="k"></ProductLine>
 
     <div class="cart">
       {{ cart.length }}
@@ -12,15 +13,30 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Product from "../components/Product.vue";
+import ProductLine from "../components/ProductLine.vue";
+import { ProductModel } from '../models';
 
 @Component({
   components: {
-    Product
+    Product,
+    ProductLine
   }
 })
 export default class Home extends Vue {
+
+  products: ProductModel[] = [];
   premium = true;
   cart: string[] = [];
+
+  constructor() {
+      super();
+      const product1 = new ProductModel();
+      product1.name = "product1";
+      const product2 = new ProductModel();
+      product2.name = "product2";
+      this.products.push(product1);
+      this.products.push(product2);
+  }
 
   updateCart(product: any) {
     console.log("Adding to cart:", product);
